f8102e3591e1c982ab11e1fdf47810118f9d7748
shlomit <shlomitshifraw@gmail.com>
2023-07-28 14:49:13 +0300

    create next app,adding dependencies, setting global css file, settint types for typescript.

---
+++ app/layout.tsx
@@ -0,0 +1,22 @@
+import "./globals.css";
+
+export const metadata = {
+  title: "Flexibble",
+  description: "Showcase and discover remarkble developer projects",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body>
+        Navbar
+        <main>{children}</main>
+        Footer
+      </body>
+    </html>
+  );
+}
