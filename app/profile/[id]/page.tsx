023f71958e82a17aacc42287ed5e9ee54a12c26f
shlomit <shlomitshifraw@gmail.com>
2023-11-19 16:16:23 +0200

    adding a project image

---
+++ app/profile/[id]/page.tsx
@@ -0,0 +1,20 @@
+import { UserProfile } from "@/common.types";
+import { getUserProjects } from "@/lib/actions";
+import ProfilePage from "@/components/ProfilePage";
+
+type Props = {
+    params:{
+        id: string;
+    }
+}
+const page = async({params}: Props) => {
+    const result = await getUserProjects(params.id, 100) as {user: UserProfile};
+
+    if(!result?.user){
+        return <p className="no-result-text">Failed to fetch user info</p>
+    }
+
+  return <ProfilePage user={result?.user}/>
+}
+
+export default page
